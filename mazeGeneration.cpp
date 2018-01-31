a7af13f0c1efcd94358d0c116e6caf88d5f1ae0d
Angus Simpson <simps110@uni.coventry.ac.uk>
2018-01-31 22:50:13 +0000

    testing 2D arrays

--- mazeGeneration.cpp
+++ mazeGeneration.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 int main()
 {
+    int mazeGeneration [4] [5] = {{1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7},{4,5,6,7,8}};
     
+    for ( int row = 0; row < 4; row++)
+    {
+        for (int column = 0; column < 3; column++)
+        {
+            cout << mazeGeneration[row][column] << " ";
+        }
+        cout << endl;
+    }
 }
