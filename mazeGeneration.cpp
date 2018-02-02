28be25a85039d8e42b1a557e21d806a2cc056f82
Angus Simpson <simps110@uni.coventry.ac.uk>
2018-02-02 10:59:26 +0000

    maze stuff

--- mazeGeneration.cpp
+++ mazeGeneration.cpp
@@ -3,11 +3,20 @@ using namespace std;
 
 int main()
 {
-    int mazeGeneration [4] [5] = {{1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7},{4,5,6,7,8}};
+    
+    
+    
+    int mazeGeneration [4] [5] = 
+    {
+        {1,2,3,4,5},
+        {2,3,4,5,6},
+        {3,4,5,6,7},
+        {4,5,6,7,8}
+    };
     
     for ( int row = 0; row < 4; row++)
     {
-        for (int column = 0; column < 3; column++)
+        for (int column = 0; column < 5; column++)
         {
             cout << mazeGeneration[row][column] << " ";
         }
