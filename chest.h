44c70428a2f98629d1aab609494d0d2e59d3a631
jsenior10 <32003487+jsenior10@users.noreply.github.com>
2018-02-26 17:30:29 +0000

    Create chest.h

---
+++ chest.h
@@ -0,0 +1,13 @@
+#ifndef CHEST_H
+#define CHEST_H
+#include <vector>
+class Chest {
+    public:
+    int xPos;
+    int yPos;
+    int Health;
+    int maxHealth;
+    bool isOpen;
+    Chest(int, int, int, int);
+};
+#endif
