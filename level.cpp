39e7085b4509cad123fde2cd7336bb7b08d78533
jsenior10 <32003487+jsenior10@users.noreply.github.com>
2018-02-26 14:35:24 +0000

    moved movement to player.cpp

--- level.cpp
+++ level.cpp
@@ -213,30 +213,6 @@ void Level::movePlayerTo(int x, int y) {
     player->yPos = y;
 }
 
-bool Level::playerMove(char direction) { //Returns true if valid move, false if not
-    int x = player->xPos;
-    int y = player->yPos;
-    vector< vector<char> > map = getMapWithEntities();
-    if(direction == 'U' && map[y-1][x] == ' ') { //up
-	player->yPos = y-1;
-	return true;
-    }
-    else if(direction == 'L' && map[y][x-1] == ' ') { //left
-        player->xPos = x-1;
-        return true;
-    }
-    else if(direction == 'D' && map[y+1][x] == ' ') { //down
-        player->yPos = y+1;
-        return true;
-    }
-    else if(direction == 'R' && map[y][x+1] == ' ') { //right
-        player->xPos = x+1;
-        return true;
-    }
-    return false;
-}
-
-
 bool Level::isEntity(int x, int y) {
     if(x == player->xPos && y == player->yPos) {
 	return true;
