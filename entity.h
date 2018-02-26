36dd243745aef361ce310af5a55348293e000d7a
jsenior10 <32003487+jsenior10@users.noreply.github.com>
2018-02-26 13:37:18 +0000

    Create entity.h

---
+++ entity.h
@@ -0,0 +1,14 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <vector>
+
+using namespace std;
+
+class Entity { // this is the enitity properties like enemy position etc
+public:
+    int xPos;
+    int yPos;
+};
+
+#endif
