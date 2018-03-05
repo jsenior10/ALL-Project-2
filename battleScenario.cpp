2b491afbdb59ea5afd346b17b7f555106e4a0e20
Jacob Senior <jacobsenior4@gmail.com>
2018-03-05 18:46:34 +0000

    dfssd

---
+++ battleScenario.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <ncurses.h>
+using namespace std;
+int mage(){
+addstr( "\n                  o\n");
+addstr( "                   O       ||`-.__\n") ;
+addstr( "                          |  |·'^|\n" );
+addstr( "             o           T    l  *\n" );
+addstr( "                        _|-..-|_\n") ;
+addstr( "                 O    (^ '----' `)\n") ;
+addstr( "                       `|-....-|^\n") ;
+addstr( "             O       o  ) */ * (\n") ;
+addstr( "                       _( (-)  )_\n") ;
+addstr( "                   O  || )    (| |\n") ;
+addstr( "                     |  |(    )|  |\n") ;
+addstr( "                 o  | o  |)  (|    |\n") ;
+addstr( "                   |     |(  )|      |\n" );
+addstr( "                  |    o | )( |       |\n") ;
+addstr( "            __.--'   O    |_|   .._   |\n") ;
+addstr( "           |||)|      ,   (_)   |(((|^)'|\n" );
+addstr( "              |       | O         )  `  |\n" );
+addstr( "              |      | o___      |      |\n") ;
+addstr( "             |  _.-''^^__O_^^''-._     |\n") ;
+addstr( "           .'  |  -''^^    ^^''-  |--'^\n") ;
+addstr( "         ..   .`.  `'''----'''^  .`. |\n" );
+addstr( "       .'    |   `'--..____..--'^   | |\n") ;
+addstr( "      |  _.-|                        | |\n" );
+addstr( "  .::'_|^   |                        |  `.\n") ;
+addstr( "         .-'|                        |    `-.\n") ;
+addstr( "   _.--'`   |                        |       `-.\n") ;
+addstr( "  |          |                      |           `-._\n") ;
+addstr( "  `'---..__   `.                  .´_.._   __       |\n" );
+addstr( "           ``'''`.              .'     `'^  `''---'^\n") ;
+addstr( "                  `-..______..-'\n");
+return 0;
+}
+int warrior()
+{
+addstr( "\n                   (    )\n");
+addstr( "                  ((((()))\n");
+addstr( "                   |o\\ //o)|\n");
+addstr( "                  ( (  _')\n");
+addstr( "                  (._.  //\\__\n");
+addstr( "                  ,\\___,/ '  ')\n");
+addstr( "    '.,_,,       (  .- .   .    )\n");
+addstr( "     \\   \\     ( '        )(    )\n");
+addstr( "      \\   \\    \\.  _.______( . )\n");
+addstr( "       \\  /\\   .(   .'//\\  ')\n");
+addstr( "        \\(  \\.-' ( /   \\//   \\)\n");
+addstr( "         '  ()) _'.-|//\\//\\//\\|\n");
+addstr( "             '\\ .( |\\//\\//\\//|\n");
+addstr( "               '((  \\    //\\    //\n");
+addstr( "               ((((  '.__\\//__.')\n");
+addstr( "                 ((,) /   ((()   )\n");
+addstr( "                 '..-,  (()('   /\n");
+addstr( "                  _//.   ((() .\n");
+addstr( "          _____ //,/' ___ ((( ', ___\n");
+addstr( "                           ((  )\n");
+addstr( "                            / /\n");
+addstr( "                          _/,/'\n");
+addstr( "                          /,/,\n");
+    return 0;
+}
+int hunter(){
+addstr( "                                    /) \n"); 
+addstr( "                                   /| \n"); 
+addstr( "                 __*_             /| \n"); 
+addstr( "              |-(____)           /| \n"); 
+addstr( "             ||||- -||          /| \n"); 
+addstr( "          ,____o% -,_          /| \n"); 
+addstr( "         |  ||   |||  ;       /| \n"); 
+addstr( "        |____|....::.||      /| \n"); 
+addstr( "       _|__|#|_ _,,_|--|    /| \n"); 
+addstr( "       |___|######## ||""-(|<| \n"); 
+addstr( "      _|__| '#######  ""^(|<| \n"); 
+addstr( "    __| |   ,)))=:=(,    || \n"); 
+addstr( "   |,--|   |Q...... |.  (|) \n"); 
+addstr( "    |      .Q......|..| \n"); 
+addstr( "          |.Q ....|...| \n"); 
+addstr( "         |.......|.....| \n"); 
+addstr( "         |.....|  |.....| \n");
+addstr( "         |_.._.|   |.._.| \n");
+addstr( "          (` )      (` ) \n"); 
+addstr( "          | |        | | \n"); 
+addstr( "          '(          )' \n"); 
+addstr( "         |+|          |+|\n "); 
+addstr( "         |,|          |,| \n"); 
+    return 0;
+}
+int dragQueen(){  
+addstr( "\n          _(,__           __),\n");
+addstr( "    (_,d888888888b,d888888888b\n");
+addstr( "     d888888888888|888888888888b_)\n");
+addstr( "  (_8888888P''''`Y8Y`'''''Y88888b\n");
+addstr( "    Y8888P.-'     `      '-.Y8888b_)\n");
+addstr( "   ,_Y88P (_(_(        )_)_) d88Y_,\n");
+addstr( "    Y88b,  (o  )      (o  ) d8888P\n");
+addstr( "    `Y888   '-'        '-'  `88Y`\n");
+addstr( "    ,d|O|         c         |O|b,\n");
+addstr( "      |*|'.,______w______,.'|*|\n");
+addstr( "         .-`             `-.\n");
+addstr( "        |   , d88b  d88b_   |\n");
+addstr( "       |   | 88888bd88888`|  |\n");
+addstr( "      |   |  | Y88888888Y  |  |\n");
+addstr( "      |   |  |  88888888   |  |\n");
+addstr( "      `| `. | d8888888b, ||||\n");
+addstr( "         `.||.d8888888888b; |\n");
+addstr( "           ||d888888888888b|\n");
+addstr( "           d8888888888888888b\n");
+addstr( "        ,_d88p""q88888p""q888b,\n");
+addstr( "         '''`|    '`|    |'`''\n");
+addstr( "              `.    |===|\n");
+addstr( "                >   |   |\n");
+addstr( "                |   |   |\n");
+addstr( "               |    |   |\n");
+addstr( "               |    Y  |\n");
+addstr( "               |   |  |\n");
+addstr( "               | || |\n");
+addstr( "               | | | |\n");
+addstr( "              |=|  |=|\n");
+addstr( "             `''`  `''`\n");
+    return 0;
+}
+//int main()
+//{ 
+   //cout << mage() << endl;
+//<<<<<<< HEAD
+    //cout << dragQueen()<<endl;
+    //cout << hunter() <<endl;
+//=======
+    //cout << warrior() << endl;
+    //cout << hunter() << endl;
+//>>>>>>> 2cdd4f3bb24af17b78b8f56b56929222eb1baa4d
+   // return 0;
+//}
---
+++ loginForm.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+#include "libsqlite.hpp"
+#include "logTableFunction.h"
+using namespace std;
+bool login(){
+    string username;
+    string password;
+    bool checking = false;
+    while(checking != true){ //the code will run untill the user type the correct passsword
+        sqlite::sqlite db( "dungeonCrawler.db" ); // open database
+        auto cur = db.get_statement(); // create query
+        bool checkUsername = false;
+        bool checkPass = false;
+        while(checkUsername != true){
+            cout << "Who are you ?" << endl;
+            cin >> username;
+            cur->set_sql("SELECT * FROM users WHERE username=? LIMIT 1"); //query
+            cur->prepare(); // run query
+            cur->bind(1, username); //fill the placeholders
+            if(cur->step()){
+                checkUsername = true;
+            }
+        }
+        while(checkPass != true){
+            cout << "Please type the magic word !" << endl;
+            cin >> password;
+            if (password.compare(cur->get_text(2)) == 0){
+                cout << "\ndone"<< endl;
+                checkPass = true;
+                checking = true;
+                int id = cur->get_int(0); 
+                insertLogTable(id,"Log in");
+            }else{
+                cout<<"Wrong password !"<< endl;
+            }
+        }
+    }
+    return true;
+}
+bool regist(){
+    string username;
+    string password = "";
+    string password2 = "";
+    bool check = true;
+    
+    try {
+        sqlite::sqlite db( "dungeonCrawler.db" ); // open database
+        cout<< "Who you wanna be ?"<< endl;
+        cin >> username;
+        while (check){   //will run untill both passwords match 
+            cout << "Set your magic word !" << endl; //receive the password and replace by *
+            cin >> password;
+            cout << "Type your magic word again !" << endl;   //receive the password2 and replace by *
+            cin >> password2;
+            if(password == password2){
+                cout<<"done"<< username << password << endl;
+                check = false;
+                auto cur = db.get_statement(); // create query
+                cur->set_sql("INSERT INTO users(username,password,level,gold) VALUES (?,?,0,100);");
+                cur->prepare(); // run query
+                cur->bind( 1, username );
+                cur->bind( 2, password );
+                cur->step();
+                cur = db.get_statement();
+                cur->set_sql("SELECT idUser FROM users WHERE username=?");
+                cur->prepare();
+                cur->bind(1,username);
+                cur->step();
+                int id2 = cur->get_int(0);
+                insertLogTable(id2,"Log in");
+                return true;
+            }else{
+                cout << "password doesn't match. " << endl;
+            }
+
+        }
+    }catch (sqlite::exception e){
+        std::cerr << e.what() << std::endl;
+        return true;
+    }
+    return false;
+    
+    
+}
+int main(){
+    int choice;
+    bool checking =false;
+    while (checking != true){
+        cout << "1-login \n"<< endl;
+        cout << "2-regist" << endl; 
+        cin >> choice;
+        if (choice == 1){
+            checking = true;
+            login();
+        }
+        else if(choice == 2){
+            checking = true;
+            regist();
+        }
+        else{
+            cout << "Please type an available choice" << endl;
+            checking = false;
+        }
+    }
+    return 0;
+}
