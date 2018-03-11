2157373ab4254ad9f50be7ed8a39b98ba9d6120a
jsenior10 <32003487+jsenior10@users.noreply.github.com>
2018-03-11 19:41:37 +0000

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -1,11 +1,12 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
+#include <vector>
 #include "entity.h"
 
 class Enemy : public Entity { // class set ups for enemies
-public:
-
+    public:
+    Enemy(int, int, int);
 };
 
 #endif
