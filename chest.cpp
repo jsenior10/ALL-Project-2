7046c11b162f99911fd1d9a8e700a4da5b37915c
jsenior10 <32003487+jsenior10@users.noreply.github.com>
2018-03-11 19:40:23 +0000

    Update chest.cpp

--- chest.cpp
+++ chest.cpp
@@ -3,23 +3,8 @@
 #include "utils.h"
 #include "chest.h"
 
-<<<<<<< HEAD
-Chest::Chest(int x, int y) {
-=======
-Chest::box(int x, int y, int HP, int max) {
->>git >>>>> 4a9044d519534e63e289cfab76d5fbde129de4a6
+Chest::Chest(int x, int y, int HP) {
     xPos = x;
     yPos = y;
-    bool isOpen = false;
-};
-<<<<<<< HEAD
-=======
-
-Chest::puzzle(int x,int y,int HP, int max)
-{
-	xPos = x;
-    yPos = y;
-    Health = HP;
-    maxHealth = max;
-};
->>>>>>> 4a9044d519534e63e289cfab76d5fbde129de4a6
+    currentHealth = HP;
+}
