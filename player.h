90a88eb5e2886e87321a3b562a444751d67fb0d0
jsenior10 <32003487+jsenior10@users.noreply.github.com>
2018-03-11 19:47:47 +0000

    Update player.h

--- player.h
+++ player.h
@@ -5,8 +5,10 @@
 
 class Player : public Entity {
 public:
-    Player(int, int); // these parameters are just for the position
-    
+    Player(int, int, int); // these parameters are just for the position
+    int chestOpen = 1; // i already set the chest to have health and i cant be bothered to change it so i just made opening it run on a damage system where you go decrease the chests health to 0 and then it opens ths chest
+    int startBattle = 1;
+    int startPuzzle = 1;
 };
 
 #endif
