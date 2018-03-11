f0f8bea9253c9b0255f434687d035845b933a8a8
jsenior10 <32003487+jsenior10@users.noreply.github.com>
2018-03-11 19:40:40 +0000

    Update chest.h

--- chest.h
+++ chest.h
@@ -1,25 +1,11 @@
 #ifndef CHEST_H
 #define CHEST_H
-#include "entity.h"
-#include <vector>
-class Chest : Entity{
-    public:
-<<<<<<< HEAD
-    bool isOpen;
-    Chest(int, int);
-=======
-    
-    int xPos;
-    int yPos;
-    int Health;
-    int maxHealth;
-    bool isOpen;
-    
-    private:
-    
-    int box(int,int,int,int);
-	int puzzle(int,int,int,int);
->>>>>>> 4a9044d519534e63e289cfab76d5fbde129de4a6
-};
 
+#include <vector>
+#include "entity.h"
+
+class Chest : public Entity {
+    public:
+    Chest(int, int, int);
+};
 #endif
