6175b0fea91ce9258cba1d093d501c812b57314e
jsenior10 <32003487+jsenior10@users.noreply.github.com>
2018-03-16 07:13:25 +0000

    Update display.h

--- display.h
+++ display.h
@@ -4,17 +4,8 @@
 #include <string>
 
 class Display {
-private:
-    bool isDeathScreenOpen = false; // some ad setupfor a screen for when the player dies
-    string currentFocus = "level";
-    
 public:
-    void open(string); //Open a window (inventory, death screen)
-    void close(string); //Close a window
-    void changeFocus(string); //Change focus to an open window to control it
-    string getFocus(); //Returns the current focus
-    void moveSelection(char); //Moves the selection in a window
-    void displayLevel(Level); //Displays the level (and whatever focus)
+    void displayLevel(Level); //Displays the level
 };
 
 #endif
