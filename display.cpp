b0de9bf9ab79cd06bb7c427eea7c91e1e4d9b640
jsenior10 <32003487+jsenior10@users.noreply.github.com>
2018-03-16 07:13:07 +0000

    Update display.cpp

--- display.cpp
+++ display.cpp
@@ -4,38 +4,6 @@
 #include "level.h"
 #include "display.h"
 
-void Display::open(string s) {
-    if(s == "death_screen") { //deathscreen is just setup incase we want to have a specific screen show up when the player dies
-	isDeathScreenOpen = true;
-    }
-}
-
-void Display::close(string s) {
-    if(s == "death_screen") {
-	isDeathScreenOpen = false;
-    }
-}
-
-void Display::changeFocus(string s) {
-    if(s == "death_screen") {
-	currentFocus = "death_screen";
-    }
-}
-
-string Display::getFocus() {
-    return currentFocus;
-}
-
-void Display::moveSelection(char direction) {
-    switch(direction) {
-	case 'L':
-	    //Do something
-	    break;
-	case 'R':
-	    //Do something
-	    break;
-    }
-}
 
 void Display::displayLevel(Level level) {
     vector< vector<char> > map = level.getMapWithEntities(); // getMapWithEntities set up for later when there are enemies and other things to dsiplay, right now it just prints the level without any enemies oe anythiong
@@ -59,9 +27,7 @@ void Display::displayLevel(Level level) {
 	nextScreen[nextScreen.size()-1][i] = ' ';
     }
     nextScreen[nextScreen.size()-1][nextScreen[0].size()-1] = ' ';
-    //Add windows over this
 
-    
     for(int i=0; i<nextScreen.size(); i++) {
 	for(int j=0; j<nextScreen[0].size(); j++) {
 	    cout << nextScreen[i][j] << ' ';
